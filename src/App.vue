3ce028c526dc2b24d782b35e1e33d2c082f9832c
cris <cristiancala@protonmail.com>
2022-12-05 01:17:02 -0400

    [vue,css]Add base files

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <h3>Interpolación de texto</h3>
-      </div>
-      <div class="bg-vue container text-center col-12">
-        <button
-          class="btn btn-hover btn-vue"
-          v-on:click="conteo++"
-        >
-          Contar
-        </button>
-        <br>
-        Me has presionado <span class="badge bg-dark">{{conteo}}</span> veces
-      </div>
+    <div class="bg-vue container text-center mt-5">
+      <button
+        class="btn btn-hover btn-vue"
+        v-on:click="conteo++"
+      >
+        Contar
+      </button>
+      <br>
+      Me has presionado <span class="badge bg-dark">{{conteo}}</span> veces
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   data() {
@@ -28,20 +22,4 @@ export default {
     }
   }
 };
-
 </script>
-
-<style scoped>
-  .color-vue {
-    color: #42b883;
-  }
-  .btn-vue {
-    background-color: white;
-    color: #42b883;
-    border:#42b883 1px solid;
-  }
-  .btn-hover:hover {
-    background-color: #42b883;
-    color: #fff;
-  }
-</style>
---
+++ src/components/Interpolation.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container mt-5">
+        <h3 class="fw-bold">Interpolación de texto</h3>
+        <p class="fw-bold">Renderizado de texto: 
+            <span class="badge bg-dark">{{ mensage }}
+            </span>
+        </p>
+        <p class="fw-bold">Renderizado de número: 
+            <span class="badge bg-dark">{{ numero }}</span>
+        </p>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'App',
+        data() {
+            return {
+                mensage: 'Hola mundo con Vue.js',
+                numero: 22
+            }
+        }
+    };
+</script>
